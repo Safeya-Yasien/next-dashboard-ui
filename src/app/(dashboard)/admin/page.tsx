757457969b65313ccaf969dbcf6439ad7a23e611
Safeya-Yasien <ssafeya5@gmail.com>
2024-09-13 10:41:15 +0300

    add line chart

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,6 @@
 import AttendanceChart from "@/components/AttendanceChart";
 import CountChart from "@/components/CountChart";
+import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
 import React from "react";
 
@@ -21,7 +22,9 @@ const AdminPage = () => {
             <AttendanceChart />
           </div>
         </div>
-        <div>bottom chart</div>
+        <div className="w-full h-[500px]">
+          <FinanceChart/>
+        </div>
       </div>
       <div className="w-full lg:w-1/3">r</div>
     </div>

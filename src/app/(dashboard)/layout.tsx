5ffd6d8f5905e20e04a26309e0f4c2c96e4d1358
Safeya-Yasien <ssafeya5@gmail.com>
2024-09-13 08:14:32 +0300

    add dashboard navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,7 @@
-
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function DashboardLayout({
   children,
@@ -6,8 +9,20 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-        <div>dashboard{children}</div>
-    </html>
+    <div className="flex h-screen">
+      <div className=" w-1/6 md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+        <Link
+          href="/"
+          className="flex items-center justify-center lg:justify-start gap-2"
+        >
+          <Image src="/logo.png" alt="logo" width={32} height={32} />
+          <span className="hidden lg:block">ScooLama</span>
+        </Link>
+        <Menu />
+      </div>
+      <div className=" w-5/6 md:w-[92%] lg:w-[86%] bg-[#F7F8FA] overflow-scroll">
+      <Navbar/>
+      </div>
+    </div>
   );
 }

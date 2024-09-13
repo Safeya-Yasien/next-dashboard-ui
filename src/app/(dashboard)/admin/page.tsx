a994a6c776b02a6eede39f04eaf5c3fe9fa65862
Safeya-Yasien <ssafeya5@gmail.com>
2024-09-14 02:55:19 +0300

    add event calendar section

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,6 @@
 import AttendanceChart from "@/components/AttendanceChart";
 import CountChart from "@/components/CountChart";
+import EventCalendar from "@/components/EventCalendar";
 import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
 import React from "react";
@@ -23,10 +24,12 @@ const AdminPage = () => {
           </div>
         </div>
         <div className="w-full h-[500px]">
-          <FinanceChart/>
+          <FinanceChart />
         </div>
       </div>
-      <div className="w-full lg:w-1/3">r</div>
+      <div className="w-full lg:w-1/3">
+        <EventCalendar />
+      </div>
     </div>
   );
 };

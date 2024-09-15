c9043d855e81dfae46bb563c1b3d27e5fd4ae5a4
Safeya-Yasien <ssafeya5@gmail.com>
2024-09-15 22:35:59 +0300

    update configuration files

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ hostname: "images.pexels.com" }],
+    domains: ["images.pexels.com"],
   },
 };
 
